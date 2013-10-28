171340d85153cd79a01f10ec6a7af3c4812a73b1
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2013-10-27 23:53:37 -0400

    Add barebones setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyGDX',
+      version='1.0',
+      description='GAMS Data Exchange (GDX) file access',
+      author='Paul Natsuo Kishimoto',
+      author_email='mail@paul.kishimoto.name',
+      url='https://github.com/khaeru/py-gdx',
+      packages=['gdx'],
+     )
