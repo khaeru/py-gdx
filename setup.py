9adf46c7a77b1f43b58480172003c2251f68714e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-16 20:22:32 -0400

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='PyGDX',
       description='GAMS Data Exchange (GDX) file access',
       author='Paul Natsuo Kishimoto',
       author_email='mail@paul.kishimoto.name',
+      install_requires=['numpy', 'pandas', 'xray'],
       url='https://github.com/khaeru/py-gdx',
       packages=['gdx'],
       )
