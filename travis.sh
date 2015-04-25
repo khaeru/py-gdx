3530e80be40bd390c45bf2b9f4657530205e77cf
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-25 14:42:19 -0400

    Correct script

--- travis.sh
+++ travis.sh
@@ -2,9 +2,9 @@
 
 # before_install script
 
-GAMS="http://d37drm4t2jghv5.cloudfront.net/distributions/24.4.3/linux/linux_x64_64_sfx.exe"
+GAMS_URL="http://d37drm4t2jghv5.cloudfront.net/distributions/24.4.3/linux/linux_x64_64_sfx.exe"
 
-curl -O $GAMS
-unzip $GAMS
+curl -O $GAMS_URL
+unzip linux_x64_64_sfx.exe
 cd gams*_linux_x86_64_sfx/apifiles/Python
 python gdxsetup.py install
