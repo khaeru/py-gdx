fcf7a26131e0dcf2fc4b0fc7b554a0eaa4a462c8
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-25 18:57:27 -0400

    Compile test GDX file; run only Python 3.4 for debugging

--- travis.sh
+++ travis.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
-# before_install script
-
 GAMS_URL="http://d37drm4t2jghv5.cloudfront.net/distributions/24.4.3/linux/linux_x64_64_sfx.exe"
 
 before_install () {
+  # Download GAMS and install the low-level gdcxx Python package
   if ! python -m gdxcc; then
     curl -O $GAMS_URL
     unzip linux_x64_64_sfx.exe
-    mv --verbose gams24.4_linux_x64_64_sfx/* $HOME/gams/
+    # The command 'mv blah $HOME/gams' does not work here; because $HOME/gams is
+    # cached, Travis creates an empty directory at that location when no cached
+    # content exists, and 'mv' places the unzipped gams code one level too deep.
+    # Move all the files instead.
+    mv gams24.4_linux_x64_64_sfx/* $HOME/gams/
     cd $HOME/gams/apifiles/Python/api
     python gdxsetup.py install
   fi
 }
 
 script () {
-  ls -l $HOME/gams
-  which -a gams
-  echo "$PATH"
-  gams
+  # Prepare data
+  pushd data
+  gams tests
+  popd
+  # Run tests
   python setup.py test
 }
 
