2cdc1a83d8992f31257fdb612e24699aa6c83d8d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-25 18:08:21 -0400

    Debug 3

--- travis.sh
+++ travis.sh
@@ -4,10 +4,22 @@
 
 GAMS_URL="http://d37drm4t2jghv5.cloudfront.net/distributions/24.4.3/linux/linux_x64_64_sfx.exe"
 
-if ! python -m gdxcc; then
-  curl -O $GAMS_URL
-  unzip linux_x64_64_sfx.exe
-  mv gams*_linux_x64_64_sfx gams
-  cd gams/apifiles/Python/api
-  python gdxsetup.py install
-fi
+before_install () {
+  if ! python -m gdxcc; then
+    curl -O $GAMS_URL
+    unzip linux_x64_64_sfx.exe
+    mv gams*_linux_x64_64_sfx gams
+    cd gams/apifiles/Python/api
+    python gdxsetup.py install
+  fi
+}
+
+script () {
+  PATH=$HOME/gams:$PATH
+  python setup.py test
+}
+
+case $1 in
+  before_install) before_install;;
+  script) script;;
+esac
