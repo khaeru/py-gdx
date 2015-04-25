0caeba46c2e07272feebf5d5dd8087471e397c21
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-25 15:37:34 -0400

    Try to set PATH

--- travis.sh
+++ travis.sh
@@ -7,7 +7,7 @@ GAMS_URL="http://d37drm4t2jghv5.cloudfront.net/distributions/24.4.3/linux/linux_
 if ! python -m gdxcc; then
   curl -O $GAMS_URL
   unzip linux_x64_64_sfx.exe
-  ln -s gams*_linux_x64_64_sfx/gams
-  cd gams*_linux_x64_64_sfx/apifiles/Python/api
+  mv gams*_linux_x64_64_sfx gams
+  cd gams/apifiles/Python/api
   python gdxsetup.py install
 fi
