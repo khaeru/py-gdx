d917de2ab84b0894cd3575a6de1fa9a51dd356c5
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2015-04-25 15:20:47 -0400

    Check for cached gdxcc package

--- travis.sh
+++ travis.sh
@@ -4,7 +4,9 @@
 
 GAMS_URL="http://d37drm4t2jghv5.cloudfront.net/distributions/24.4.3/linux/linux_x64_64_sfx.exe"
 
-curl -O $GAMS_URL
-unzip linux_x64_64_sfx.exe
-cd gams*_linux_x64_64_sfx/apifiles/Python/api
-python gdxsetup.py install
+if ! python -m gdxcc; then
+  curl -O $GAMS_URL
+  unzip linux_x64_64_sfx.exe
+  cd gams*_linux_x64_64_sfx/apifiles/Python/api
+  python gdxsetup.py install
+fi
